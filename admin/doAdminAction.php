a3951d76daeff160c67792fca785019be54782ae
SunDoge <384813529@qq.com>
2016-02-01 22:58:02 +0800

    fix delete

--- admin/doAdminAction.php
+++ admin/doAdminAction.php
@@ -1,14 +1,7 @@
 <?php
 require_once '../include.php';
 $act = $_REQUEST['act'];
-$id = $_REQUEST['id'];
-/*
-if($act == "logout"){
-	logout();
-}elseif ($act == "addAdmin") {
-	$mes = addAdmin();
-}
-*/
+
 switch ($act) {
 	case 'logout':
 		logout();
@@ -19,7 +12,11 @@ switch ($act) {
 		break;
 
 	case 'editAdmin':
-		$mes = editAdmin();
+		$mes = editAdmin($_REQUEST['id']);
+		break;
+
+	case 'delAdmin':
+		$mes = delAdmin($_REQUEST['id']);
 		break;	
 }
 ?>
